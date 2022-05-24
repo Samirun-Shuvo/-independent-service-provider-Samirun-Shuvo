aed6d197b22101f19de10ece8166d426a28f9f7f
excelitai-mahedi <mahedi@excelitai.com>
2022-05-24 17:19:07 +0600

    email verification link sent update

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,31 +1,42 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
 import auth from "./../../../firebase.init";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Register = () => {
   const [agree, setAgree] = useState(false);
+
   const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
   const navigate = useNavigate();
   const navigateLogin = (event) => {
     navigate("/login");
   };
   if (user) {
-    navigate("/home");
+    console.log("user", user);
   }
-  const handleRegister = (event) => {
+  const handleRegister = async (event) => {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
     // const agree =event.target.terms.checked;
     // console.log(name,email,password);
-    if (agree) {
-      createUserWithEmailAndPassword(email, password);
-    }
+    // if (agree) {
+    //   createUserWithEmailAndPassword(email, password);
+    // }
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name });
+    console.log("Updated profile");
+    navigate("/home");
   };
   return (
     <div className="container">
